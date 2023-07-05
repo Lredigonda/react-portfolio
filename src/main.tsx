641fd1ff4c46e6faaae20534e11c044ba8ac825e
Lredigonda <leoredigonda@hotmail.com>
2023-07-05 18:41:00 -0300

    Fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,33 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import About from "./pages/about";
+import { Curriculum } from "./pages/curriculum.tsx";
+import { HomePage } from "./pages/home-page";
+import { Projects } from "./pages/projects.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "curriculum",
+        element: <Curriculum />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+    ],
   },
 ]);
 
